192a011940e0bd114b336cdeb89ca5ed0acc4e32
adolfo <adolfocn92@gmail.com>
2022-08-01 15:36:49 -0400

    Ejercicio 4 todos los check listos

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function(array,b) {
 
     for(itemIndex = 1; itemIndex<arguments.length;itemIndex++){
 
-        indexOfRemoved = array.findIndex((find)=>find==arguments[itemIndex]);
+        indexOfRemoved = array.findIndex((find)=>find===arguments[itemIndex]);
 
         if (indexOfRemoved == -1) continue; //Si no consigue nada el metodo .findIndex() retorna -1
 
