02760009d4835e1057d40bfec22d09dfc8ae7816
adolfo <adolfocn92@gmail.com>
2022-09-08 08:07:26 -0400

    Finish Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (texto) {
+
+    const textoLimpio = texto.replace(/[!,. ]/g,'').toLowerCase() //Quitame los espacios, las comas y los signos de admiracion luego ponlo todo en minuscula
+
+    const textoReverso = textoLimpio.split('').reverse().join('');
+
+    if(textoLimpio===textoReverso) return true;
+
+    return false;
 
 };
 
