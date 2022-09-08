88bcff4da902ca2e304154945e39a37291980637
adolfo <adolfocn92@gmail.com>
2022-09-08 13:54:11 -0400

    Finish find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfPeople) {
+
+    const currentYear = (new Date()).getFullYear();
+
+    const x = arrayOfPeople.sort(function(persona1,persona2){
+        
+        if(!persona1.yearOfDeath){
+            persona1.yearOfDeath = currentYear;
+        }
+        if(!persona2.yearOfDeath){
+            persona2.yearOfDeath = currentYear;
+        }
+
+        let pOldness1 =persona1.yearOfDeath - persona1.yearOfBirth;
+        let pOldness2 =persona2.yearOfDeath - persona2.yearOfBirth;
+
+        return pOldness2-pOldness1;
+    })
+
+    return x[0];
+    
 
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
